48ed60366c5a3a07360c0710051eb94203dde8ea
YeungHow <yhtan.eng@gmail.com>
2020-12-07 08:50:04 +0800

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+$(document).ready(function(){
+    $("#carouselButton").click(function(){
+        $("#mycarousel").carousel( { interval: 2000 } );
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+
+    $('#loginButton').click(function() {
+        $('#loginModal').modal('show');
+    });
+
+    $('#reserveButton').click(function() {
+        $('#reserveModal').modal('show');
+    });
+        
+    
+});
